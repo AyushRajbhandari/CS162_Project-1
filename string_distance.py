ccd119966551dcb806c6aaa21b7c7f82d6acef5f
Ayush Rajbhandari <ayushrajbhandari06@gmail.com>
2026-06-24 23:56:37 -0700

    Refactor string_distance function parameters
    Updated function parameters and docstring for clarity.

--- string_distance.py
+++ string_distance.py
@@ -1,18 +1,21 @@
-# Author: Ayush Rajbhandari
+# Name: Ayush Rajbhandari
 # Date: 6/24/26
 # Assignment: Project 1
 # Description: This script contains a function that calculates the Levenshtein distance similarity ratio between a list of strings and a target string.
 
 from fuzzywuzzy import fuzz
 
-def string_distance(str1, str2):
-    # takes a list of strings, and a target string, returning a dictionary where the keys are the strings from the list and the values are the similarity ratios
+def string_distance(string_list, target_string):
+    """
+    Takes a list of strings and a target string, and returns a dictionary where the keys are the strings
+    from the list, and the values are the similarity ratio
+    """
 
     similarity_dictionary = {}
 
-    for list_item in str1:
+    for list_item in string_list:
 
-        similarity_score = fuzz.ratio(list_item, str2) #calculates similarity ratio between current list and target list
+        similarity_score = fuzz.ratio(list_item, target_string) #calculates similarity ratio between current list and target list
 
         similarity_dictionary[list_item] = similarity_score #adds to dictionary list
 
